9dc988c3002b39f0bd5d92fbc522d753ccfacf18
L Chen <jenac@outlook.com>
2015-05-09 23:33:58 -0500

    add missing file

---
+++ StockWatch.Entities/Table/CompanyMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockWatch.Entities.Table
+{
+    public class CompanyMapping : EntityTypeConfiguration<Company>
+    {
+        public CompanyMapping()
+        {
+            this.HasKey(e => new { e.Symbol, e.Exchange });
+            this.Property(e => e.Symbol).HasMaxLength(16);
+            this.Property(e => e.Exchange).HasMaxLength(16);
+            this.Property(e => e.Name).HasMaxLength(256);
+            this.Property(e => e.Sector).HasMaxLength(256);
+            this.Property(e => e.Industry).HasMaxLength(256);
+
+        }
+    }
+}
