89af225cea16d5978f9d32c94b7cd3b6d16f3980
L Chen <jenac@outlook.com>
2015-06-07 09:05:51 -0500

    Add Encryption

---
+++ StockWatch.Utility.Test/EncryptionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StockWatch.Utility.Test
+{
+    [TestClass]
+    public class EncryptionTest
+    {
+        [TestMethod]
+        public void Encryption_can_encrypt_string()
+        {
+            string plain = "Aa1234%^&*()-+";
+            string pwd = "Aa1234%^&*()-+";
+            string encrypted = Encryption.Encrypt(plain, pwd);
+            string decrypted = Encryption.Decrypt(encrypted, pwd);
+            Assert.IsTrue(encrypted == plain);
+
+        }
+    }
+}
---
+++ StockWatch.Utility/Encryption.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockWatch.Utility
+{
+    public static class Encryption
+    {
+        public static string Encrypt(string text, string pwd)
+        {
+            byte[] originalBytes = Encoding.UTF8.GetBytes(text);
+            byte[] encryptedBytes = null;
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(pwd);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            // Generating salt bytes
+            byte[] saltBytes = GetRandomBytes();
+
+            // Appending salt bytes to original bytes
+            byte[] bytesToBeEncrypted = new byte[saltBytes.Length + originalBytes.Length];
+            for (int i = 0; i < saltBytes.Length; i++)
+            {
+                bytesToBeEncrypted[i] = saltBytes[i];
+            }
+            for (int i = 0; i < originalBytes.Length; i++)
+            {
+                bytesToBeEncrypted[i + saltBytes.Length] = originalBytes[i];
+            }
+
+            encryptedBytes = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static string Decrypt(string decryptedText, string pwd)
+        {
+            byte[] bytesToBeDecrypted = Convert.FromBase64String(decryptedText);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(pwd);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] decryptedBytes = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            // Removing salt bytes, retrieving original bytes
+            byte[] originalBytes = new byte[decryptedBytes.Length - _externalSaltSize];
+            for (int i = _externalSaltSize; i < decryptedBytes.Length; i++)
+            {
+                originalBytes[i - _externalSaltSize] = decryptedBytes[i];
+            }
+
+            
+
+            return Encoding.UTF8.GetString(originalBytes);
+        }
+
+        private static byte[] GetRandomBytes()
+        {
+            byte[] ba = new byte[_externalSaltSize];
+            RNGCryptoServiceProvider.Create().GetBytes(ba);
+            return ba;
+        }
+
+        private static int _externalSaltSize = 4;
+        private static byte[] _internalSalt = new byte[] { 73, 77, 04, 10, 2, 4, 27, 2, 2 };
+        private static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, _internalSalt, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, _internalSalt, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+
+
+    }
+}
